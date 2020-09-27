392246eadfc30a0f02b3f64912863663e1ea2ed0
Nico Cobelo <cobelon@gmail.com>
2020-09-27 20:04:12 -0300

    Initial commit

---
+++ dna.py
@@ -0,0 +1,12 @@
+from cs50 import get_int
+from sys import argv
+from csv import reader
+from csv import DictReader
+
+if len(argv) != 3:
+    print("Usage: python dna.py data.csv sequence.txt")
+    exit(1)
+print("Lavender")
+exit(0)
+
+    
