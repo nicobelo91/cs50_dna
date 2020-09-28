81fe62a4d8b1c73a7ddf8f6bddf112d562c23368
Nico Cobelo <cobelon@gmail.com>
2020-09-27 21:18:28 -0300

    Added sys, csv and re packages

--- dna.py
+++ dna.py
@@ -1,12 +1,38 @@
 from cs50 import get_int
-from sys import argv
-from csv import reader
-from csv import DictReader
+import sys
+import csv
+import re
 
-if len(argv) != 3:
+# Check if command line is complete
+if len(sys.argv) != 3:
     print("Usage: python dna.py data.csv sequence.txt")
-    exit(1)
-print("Lavender")
-exit(0)
+    sys.exit(1)
+# Open csv file and read into memory
+csvfile = open("small.csv","r")
+# Open dna sequence txt file, read into memory
+dnasequence = open()
 
-    
+# For each STR compute the longest run of consecutive repeats
+
+
+# Save STR counts into a data structure
+writer = csv.writer(file)
+# Compare the STR counts against each row in CSV file
+if match:
+    print("Lavender")
+else:
+    print("No Match")
+
+"""
+s = "AASDASDDAAAAAAAAERQREQREQRAAAAREWQRWERAAA"
+
+groups = re.findall(r'(?:AA)+', s)
+print(groups)
+# ['AA', 'AAAAAAAA', 'AAAA', 'AA']
+
+largest = max(groups, key=len)
+print(len(largest) // 2)
+# 4
+"""
+csvfile.close()
+dnasequence.close()
