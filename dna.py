d4536dd01cc55696717ef7efb51d2bcb659d3aa8
Nico Cobelo <cobelon@gmail.com>
2020-10-01 19:42:20 -0300

    Finished!!!!!

--- dna.py
+++ dna.py
@@ -4,45 +4,54 @@ import csv
 import re
 
 def main():
+from cs50 import get_int
+import sys
+import csv
+import re
+
+
+def main():
     # Check if command line is complete
     if len(sys.argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
         sys.exit(1)
     # Open csv file and read into memory
-    with open(f"{sys.argv[1]}",'r') as csvfile:
-        database = csv.reader(csvfile)
-        list_of_headers = next(database)[1:]
+    csvfile = open(f"{sys.argv[1]}", 'r')
+    database = csv.reader(csvfile)
+    list_of_headers = next(database)[1:]
         
-         # Open dna sequence txt file, read into memory
-        with open(f"{sys.argv[2]}",'r') as dnasequence:
-        
-            # Search in the database:
-            sequence = csv.reader(dnasequence)
-            # Save STR counts into a data structure
-            for dna in sequence:
-                # Convert dna list into a string
-                listToString = ''.join([str(elem) for elem in dna])
-                dnaGroup = [substring_length(listToString, header) for header in list_of_headers]
-                
-                print_name(database,dnaGroup)
+    # Open dna sequence txt file, read into memory
+    with open(f"{sys.argv[2]}", 'r') as dnasequence:
+    
+        # Search in the database:
+        sequence = dnasequence.read()
+        #sequence = csv.reader(dnasequence)
+        # Save STR counts into a data structure
+        dnaGroup = [substring_length(sequence, header) for header in list_of_headers]
+            
+        print_name(database, dnaGroup)
+    csvfile.close()
 
-# For each STR compute the longest run of consecutive repeats                
-def substring_length(string, substring):        
-    # Search for AGATC
-    substring = re.findall(r'(?:{substring})+', string) # Doesn't read the value of {substring}
-    #print(AGATC)
-    print(substring)
-    largest = max(substring, key=len)
-    #print(len(largestAGATC) // 5)
-    return ((len(largest) // len(substring)))
+# For each STR compute the longest run of consecutive repeats     
+
+
+def substring_length(string, substring):
+    count = 0  # Number of times that the substring is repeated
+    pattern = substring
+    while pattern in string:
+        count += 1
+        pattern += substring
+    return count
                 
-def print_name(database,dnaGroup):
+                
+def print_name(database, dnaGroup):
     for row in database:
         person = row[0]
-        values = [val for val in row[1:]]
+        values = [int(val) for val in row[1:]]
         if values == dnaGroup:
             print(person)
             return
-        print("No match")
-        
+    print("No match")
+    
+    
 main()
